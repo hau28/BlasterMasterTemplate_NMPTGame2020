20e27f4884eb8f9db0b8770ad360ce2b3a69e16e
CuteTN <quantiennghia001@gmail.com>
2020-12-15 00:55:40 +0700

    Implement Bullet_skull physics logic

--- BlasterMaster/Bullet_Skull.cpp
+++ BlasterMaster/Bullet_Skull.cpp
@@ -19,14 +19,25 @@ CBullet_Skull::CBullet_Skull(float x, float y, int sectionId, float dirX, float 
 	vx = (float)(distanceX / module * 2)/16;
 	vy =(float) (distanceY / module * 3)/16;
 
-	timestartDrop = GetTickCount();
-
-
+	explodeTimer = new CTimer(this, TIME_TO_EXPLODE, 1);
+	explodeTimer->Start();
 }
 
 void CBullet_Skull::UpdateVelocity(DWORD dt)
 {
+	vy += BULLET_SKULL_GRAVITY;
+	vy *= (1 - BULLET_SKULL_FRICTION);
 
+	// Update()
+	// vì t lười thêm update nên thôi t spam vô đây tạm nha
+	// CuteTN reply: Ok, mốt có lười thì thêm vào UpdateVelocity để nó call 1 lần thôi nha, HandleCollision với HandleOverlap nó được call nhiều lần
+	if (!checkObjInCamera(this, SCREEN_EXTEND_OFFSET_DEFAULT))
+	{
+		CGameObjectBehaviour::RemoveObject(this);
+	}
+
+	// CuteTN Note: Tao cũng lười .-.
+	explodeTimer->Update(dt);
 }
 
 void CBullet_Skull::HandleCollision(DWORD dt, LPCOLLISIONEVENT coEvent)
@@ -47,25 +58,34 @@ void CBullet_Skull::HandleCollision(DWORD dt, LPCOLLISIONEVENT coEvent)
 		case CLASS_TILE_BLOCKABLE:
 		case CLASS_TILE_PORTAL:
 		{
+			float oldVy = vy;
+
 			CGameObjectBehaviour::BlockObject(dt, coEvent);
 			
-			if (coEvent->ny != 0)
-				vy = BULLET_SKULL_SPEED;
+			// CuteTN Note: bouncing logic
+			if (coEvent->ny < 0)
+				vy = -oldVy;
+
 			break;
 		}
 		}
-	}
-
-	// vì t lười thêm update nên thôi t spam vô đây tạm nha
-	if (!checkObjInCamera(this, SCREEN_EXTEND_OFFSET_DEFAULT) || GetTickCount() - timestartDrop > TIMETOEXPLODE || CCollisionSolver::IsOverlapped(this, CGame::GetInstance()->GetCurrentPlayer()))
-	{
-		CGameObjectBehaviour::RemoveObject(this);
-		Explode(CLASS_LARGE_EXPLOSION_SIDEVIEW);
 	}
 }
 
 void CBullet_Skull::HandleOverlap(LPGAMEOBJECT overlappedObj)
 {
+	if (overlappedObj == CGame::GetInstance()->GetCurrentPlayer())
+		Explode(CLASS_LARGE_EXPLOSION_SIDEVIEW);
+}
+
+void CBullet_Skull::HandleTimerTick(CTimer* sender)
+{
+	Explode(CLASS_LARGE_EXPLOSION_SIDEVIEW);
+}
+
+CBullet_Skull::~CBullet_Skull()
+{
+	delete explodeTimer;
 }
 
 
--- BlasterMaster/Bullet_Skull.h
+++ BlasterMaster/Bullet_Skull.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "Bullet.h"
-class CBullet_Skull : public CBullet
+#include "Timer.h"
+
+class CBullet_Skull : public CBullet, public ITimeTrackable
 {
 private:
-    const float TIMETOEXPLODE = 2000;
-    DWORD timestartDrop;
+    const float TIME_TO_EXPLODE = 2000;
+
+    const float BULLET_SKULL_GRAVITY = 0.02f;
+    const float BULLET_SKULL_FRICTION = 0.04f;
+
+    CTimer* explodeTimer = nullptr;
+
 public:
     const float BULLET_SKULL_SPEED = 0.3f;
 
@@ -15,5 +22,8 @@ public:
     virtual void HandleCollision(DWORD dt, LPCOLLISIONEVENT coEvent);
     virtual void HandleOverlap(LPGAMEOBJECT overlappedObj);
 
+    virtual void HandleTimerTick(CTimer* sender);
+
+    ~CBullet_Skull();
     //void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjs);
 };
