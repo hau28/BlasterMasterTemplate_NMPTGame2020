ec712cd75506e2bc637c2ae6c09b1b653c2929f8
CuteTN <quantiennghia001@gmail.com>
2020-12-26 15:32:15 +0700

    Implement thunder break weapon

--- BlasterMaster/Sophia.cpp
+++ BlasterMaster/Sophia.cpp
@@ -10,6 +10,7 @@
 #include "Bullet_Sophia.h"
 #include "Bullet_HomingMissile.h"
 #include "Bullet_MultiwarheadMissile.h"
+#include "Bullet_ThunderBreak.h"
 #include "SwitchSceneEvent.h"
 #include "GameGlobal.h"
 
@@ -811,6 +812,13 @@ void CSophia::ShootMultiwarheadMissile()
 
 void CSophia::ShootThunderBreak()
 {
+    if (!CGameGlobal::GetInstance()->CheckSophiaCanUseWeapon())
+        return;
+
+    CBullet_ThunderBreak* bullet = new CBullet_ThunderBreak(0, 0, 0);
+    CGameObjectBehaviour::CreateObjectAtCenterOfAnother(bullet, this);
+
+    CGameGlobal::GetInstance()->AddToSelectedWeapon(-1);
 }
 
 void CSophia::HandleTimerTick(LPTIMER sender)
