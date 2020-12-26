48f9a7e7238e8062d0712176863af39672390dcd
CuteTN <quantiennghia001@gmail.com>
2020-12-26 19:14:53 +0700

    Add control for Jason overhead

--- BlasterMaster/JasonOverhead.cpp
+++ BlasterMaster/JasonOverhead.cpp
@@ -1,4 +1,10 @@
 #include "JasonOverhead.h"
+#include "TileArea.h"
+#include "Portal.h"
+#include "GameObjectBehaviour.h"
+#include "PortalLib.h"
+#include "GameGlobal.h"
+#include "WalkInPortalEvent.h"
 
 CJasonOverhead* CJasonOverhead::__instance = nullptr;
 
@@ -28,6 +34,95 @@ void CJasonOverhead::Init()
 
     //vulnerableFlashingEffect = new CObjectFlashingEffectPlayer(this, &flashingColors, JASONSIDEVIEW_VULNERABLE_EFFECT_FLASHING_DURATION);
 }
+
+void CJasonOverhead::HandleKeys(DWORD dt)
+{
+    HandleKeysHold(dt);
+
+    auto keyEvents = NewKeyEvents();
+
+    for (auto e : keyEvents)
+    {
+        int keyCode = e->GetKeyCode();
+        if (e->IsDown())
+            HandleKeyDown(dt, keyCode);
+        else
+            HandleKeyUp(dt, keyCode);
+    }
+}
+
+void CJasonOverhead::HandleKeyUp(DWORD dt, int keyCode)
+{
+}
+
+void CJasonOverhead::HandleKeyDown(DWORD dt, int keyCode)
+{
+}
+
+void CJasonOverhead::HandleKeysHold(DWORD dt)
+{
+    MovingDirX = MovingDirY = 0;
+
+    if (IsKeyDown(DIK_UP))
+        MovingDirY--;
+    if (IsKeyDown(DIK_DOWN))
+        MovingDirY++;
+    if (IsKeyDown(DIK_LEFT))
+        MovingDirX--;
+    if (IsKeyDown(DIK_RIGHT))
+        MovingDirX++;
+}
+
+bool CJasonOverhead::IsMoving()
+{
+    return (MovingDirX != 0) || (MovingDirY != 0);
+}
+
+void CJasonOverhead::UpdateState()
+{
+    int newState = state;
+
+    if (IsMoving())
+    {
+        if (MovingDirX != 0 && MovingDirY != 0)
+            ;
+        else
+        {
+            if (MovingDirX < 0)
+            {
+                newState = JASONOVERHEAD_STATE_WALK_LEFT;
+                FaceDir = JASON_OVERHEAD_FACEDIR_LEFT;
+            }
+            else if (MovingDirX > 0)
+            {
+                newState = JASONOVERHEAD_STATE_WALK_RIGHT;
+                FaceDir = JASON_OVERHEAD_FACEDIR_RIGHT;
+            }
+            else if (MovingDirY < 0)
+            {
+                newState = JASONOVERHEAD_STATE_WALK_UP;
+                FaceDir = JASON_OVERHEAD_FACEDIR_UP;
+            }
+            else if (MovingDirY > 0)
+            {
+                newState = JASONOVERHEAD_STATE_WALK_DOWN;
+                FaceDir = JASON_OVERHEAD_FACEDIR_DOWN;
+            }
+        }
+    }
+    else
+    {
+        switch (FaceDir)
+        {
+        case JASON_OVERHEAD_FACEDIR_UP: newState = JASONOVERHEAD_STATE_IDLE_UP; break;
+        case JASON_OVERHEAD_FACEDIR_DOWN: newState = JASONOVERHEAD_STATE_IDLE_DOWN; break;
+        case JASON_OVERHEAD_FACEDIR_LEFT: newState = JASONOVERHEAD_STATE_IDLE_LEFT; break;
+        case JASON_OVERHEAD_FACEDIR_RIGHT: newState = JASONOVERHEAD_STATE_IDLE_RIGHT; break;
+        }
+    }
+
+    SetState(newState);
+}
 
 CJasonOverhead* CJasonOverhead::GetInstance()
 {
@@ -51,14 +146,71 @@ CJasonOverhead* CJasonOverhead::InitInstance(int x, int y, int sectionId)
 
 void CJasonOverhead::UpdateVelocity(DWORD dt)
 {
+    vx = MovingDirX * JASON_OVERHEAD_MOVING_SPEED_X;
+    vy = MovingDirY * JASON_OVERHEAD_MOVING_SPEED_Y;
 }
 
 void CJasonOverhead::HandleCollision(DWORD dt, LPCOLLISIONEVENT coEvent)
 {
+    if (coEvent == nullptr)
+        return;
+    if (coEvent->otherObject == this)
+        return;
+
+    LPGAMEOBJECT obj = coEvent->otherObject;
+
+    if (dynamic_cast<LPTILE_AREA>(obj))
+    {
+        LPTILE_AREA tileArea = dynamic_cast<LPTILE_AREA>(obj);
+
+        switch (tileArea->classId)
+        {
+        case CLASS_TILE_BLOCKABLE:
+        {
+			CGameObjectBehaviour::BlockObject(dt, coEvent);
+            break;
+        }
+
+        case CLASS_TILE_PORTAL:
+        {
+            LPPORTAL fromPortal = dynamic_cast<LPPORTAL>(obj);
+            LPPORTAL toPortal = CPortalLib::GetInstance()->Get(fromPortal->associatedPortalId);
+
+            // Sanh code from here!
+            LPGAME_EVENT newEvent = new CWalkInPortalEvent("WalkInPortalEvent", fromPortal, toPortal);
+            CGame::GetInstance()->AddGameEvent(newEvent);
+            // to do: create an event to CGame, let CGame handle switching section
+            DebugOut(L"Jason to portal %d of section %d, tick %d\n", toPortal->associatedPortalId, toPortal->currentSectionId, GetTickCount());
+
+            break;
+        }
+
+
+        }
+    }
+
+    if (dynamic_cast<CEnemy*>(obj))
+    {
+        CGameGlobal::GetInstance()->beingAttackedByEnemy();
+    }
+
+    if (dynamic_cast<CBullet*>(obj))
+    {
+        CBullet* bullet = dynamic_cast<CBullet*>(obj);
+        if (!bullet->isFriendly)
+            CGameGlobal::GetInstance()->beingAttackedByEnemy();
+    }
 }
 
 void CJasonOverhead::HandleOverlap(LPGAMEOBJECT overlappedObj)
 {
+}
+
+void CJasonOverhead::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjs)
+{
+    HandleKeys(dt);
+    CAnimatableObject::Update(dt, coObjs);
+    UpdateState();
 }
 
 void CJasonOverhead::GetBoundingBox(float& left, float& top, float& right, float& bottom)
--- BlasterMaster/JasonOverhead.h
+++ BlasterMaster/JasonOverhead.h
@@ -4,15 +4,37 @@
 // 2 25 21 31
 
 const int JASONOVERHEAD_BOUNDBOX_WIDTH = 20;
-const int JASONOVERHEAD_BOUNDBOX_HEIGHT = 7;
+const int JASONOVERHEAD_BOUNDBOX_HEIGHT = 9;
 const int JASONOVERHEAD_BOUNDBOX_OFFSETX = 2;
 const int JASONOVERHEAD_BOUNDBOX_OFFSETY = 25;
+
+const int JASON_OVERHEAD_FACEDIR_UP = 0;
+const int JASON_OVERHEAD_FACEDIR_DOWN = 1;
+const int JASON_OVERHEAD_FACEDIR_LEFT = 2;
+const int JASON_OVERHEAD_FACEDIR_RIGHT = 3;
 
 class CJasonOverhead : public CAnimatableObject
 {
 private:
+    const float JASON_OVERHEAD_MOVING_SPEED_X = 0.1f;
+    const float JASON_OVERHEAD_MOVING_SPEED_Y = 0.1f;
+
     CJasonOverhead();
     void Init();
+
+    void HandleKeys(DWORD dt);
+    void HandleKeyUp(DWORD dt, int keyCode);
+    void HandleKeyDown(DWORD dt, int keyCode);
+    void HandleKeysHold(DWORD dt);
+
+    int MovingDirX = 0;
+    int MovingDirY = 0;
+
+    bool IsMoving();
+
+    int FaceDir = 1;
+
+    void UpdateState();
 
     static CJasonOverhead* __instance;
 
@@ -21,7 +43,7 @@ public:
     virtual void HandleCollision(DWORD dt, LPCOLLISIONEVENT coEvent);
     virtual void HandleOverlap(LPGAMEOBJECT overlappedObj);
     virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
-    // virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjs);
+    virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjs);
 
     static CJasonOverhead* GetInstance();
     static CJasonOverhead* InitInstance(int x, int y, int sectionId);
